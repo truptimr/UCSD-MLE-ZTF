f926352da3ca488cbb7d5fb3fd658f30d6e55fcf
truptimr <trupti.ranka@gmail.com>
2021-08-15 22:13:51 -0700

    add streamlit ui python scripts.

---
+++ ui_streamlit/app.py
@@ -0,0 +1,186 @@
+import pandas as pd
+import streamlit as st
+import classification_backend as cb
+import sqlalchemy as db
+
+
+web_text = """
+This is an interactive webpage which predicts the variable type of a given light curve from Zwicky Transient Facility (ZTF) database. 
+The prediction is done using XG Boost classifiers.
+The classifiers consists of 1 vs All XG Boost classifiers for 9 different types of periodic variable stars.
+The classifiers were trained using labeled ZTF DR2 data set obtained from http://variables.cn:88/
+"""
+
+
+feature_explanation = """
+			Feature list  
+			- **mean** : mean of the light curve.
+			- **wmean** : weight mean of the light curve.
+			- **MAD**: deviation about the median.
+			- **IQR** : inter quartile percentile of the light curve.
+			- **f60** : 60 percentile  of light curve.
+			- **f70** : 70 percentile of the light curve.
+			- **f80** : 80 percentile of the light curve.
+			- **f90** : 90 percentile of the light curve.
+			- **skew** : skewness of the light curve.
+			- **kurtosis** : kurtosis of the light curve.
+			- **welsk_k, welsh_j** : welsh and staton J and K statistics of the light curve.
+			- **g_**: g band filter.
+			- **r_** : r band filter.
+			"""
+
+variable_type_explanation = """
+			## Glossary of different type of variable stars
+
+			The 9 different types of variable stars in the data are as follows:  
+
+			1. **BYDra**: BY Draconis variables are variable stars of late spectral types, usually K or M, and typically belong to the main sequence. 
+			1. **EW** : EW-type eclipsing binaries (EWs) are W Ursae Majoris-type eclipsing variables with periods shorter than one day.
+			1. **SR** : semi-regular variables
+			1. **RSCVN** : An RS Canum Venaticorum variable is a type of variable star. The variable type consists of close binary stars having active chromospheres which can cause large stellar spots.
+			1. **RR** :RR Lyrae variables are periodic variable stars, commonly found in globular clusters. They are used as standard candles to measure (extra) galactic distances, assisting with the cosmic distance ladder. 
+			1. **DSCT**: A Delta Scuti variable (sometimes termed dwarf cepheid when the V-band amplitude is larger than 0.3 mag.) is a subclass of young pulsating star. These variables as well as classical cepheids 
+			1. **EA** : Algol (Beta Persei)-type eclipsing systems. Binaries with spherical or slightly ellipsoidal components. 
+			1. **Mira** : Mira variables are a class of pulsating stars characterized by very red colours, pulsation periods longer than 100 days, and amplitudes greater than one magnitude in infrared and 2.5 magnitude at visual wavelengths. 
+			1. **RRc** : RR Lyrae variable stars of subclass c. 
+			"""
+
+References = """
+Zwicky Transient Facility  
+
+1. [ZTF Website](https://www.ztf.caltech.edu/)
+1. [Jan van Roestel et al 2021 AJ 161 267] (https://iopscience.iop.org/article/10.3847/1538-3881/abe853/meta?casa_token=8xxK5DlzXYsAAAAA:kELEfby98rcABefbQtaO20Iiwf-0SojgTt1tRcuqhk--xDVv_r1O5XVv2wZHQpLOV-njnptl5A)
+1. [Xiaodian Chen et al 2020 ApJS **249** 18] (https://iopscience.iop.org/article/10.3847/1538-4365/ab9cae/meta)  
+
+Code  
+
+1. [sklearn](https://scikit-learn.org/stable/)  
+1. [streamlit](https://streamlit.io/)  
+1. [Git repo](https://github.com/truptimr/UCSD-MLE-ZTF)  
+"""
+
+Help = """
+1. Select a dataset to query.  
+1. Input light source paramaters. 
+1. Query the data.
+1. (Optional) Save light curve in a SQ Lite database locally. The table index of the light curve in the database is of the format <RA>_<Dec> where RA and dec are floats with 2 decimal places.
+1. Make a predition.
+1. Tip: Uncheck all boxes before switching between datasets
+"""
+
+## open local SQL lite database connections
+engine = db.create_engine('sqlite:///LightSource.db', echo=False)
+sqlite_connection = engine.connect()
+
+
+
+
+
+
+def prediction(lc, NA):
+	"""
+	Do prediction and display results
+	"""
+	features = cb.calculate_features(lc)
+	"""
+	## Features
+	"""
+	cols = features.columns.values
+	df1 = features.loc[:,cols[0:8]]
+	df2 = features.loc[:,cols[8:16]]
+	df3 = features.loc[:,cols[16:24]]
+	st.write(df1)
+	st.write(df2)
+	st.write(df3)
+	if st.checkbox('Feature Explanation'):
+		st.markdown(feature_explanation)
+	prob = cb.prediction_probabilty(features)
+	"""
+	## Prediction Probability
+	"""
+	probT = prob.T
+	st.write(probT.style.format("{:.2}"))
+	max_prob = prob.Probability.max()
+	if max_prob>0.5 and not(NA):
+		star = prob.idxmax(axis=0, skipna=True).values[0]
+		pred_str = '**The most likely star type for the lightcurve is ' + star + '**'
+		st.markdown(pred_str)
+	if max_prob<=0.5 or NA:
+		st.markdown('**The lightcurve does not belog to any of the 9 periodic variables types**')
+	if st.checkbox('Variable Type Explanation'):
+		st.markdown(variable_type_explanation)
+
+### The Webpage ####
+
+st.title("ZTF data variable star classifier")
+
+st.markdown(web_text)
+
+if st.checkbox('References'):
+	st.markdown(References)
+
+if st.checkbox('Help'):
+	st.markdown(Help)
+
+st.sidebar.header('Light Source Datasets')
+st.sidebar.text("""Uncheck all boxes before 
+switching between datasets""")
+
+if st.sidebar.checkbox('ZTF Dataset'):
+	st.sidebar.text('''Input source location RA and 
+DEC with 10 arcsec accuracy
+''' )
+	RA = st.sidebar.number_input('Right Acsecion (Degree)', min_value=0.0, max_value=360.0, value=72.0, step=0.01)
+	Dec = st.sidebar.number_input('Declination Acsecion (Degree)', min_value=0.0, max_value=90.0, value = 24.00, step=0.001)
+	if st.sidebar.checkbox('Query Location'):
+		lc1, lc_complete1 = cb.query_lightcurve_DR(RA, Dec)
+		if lc1.empty:
+			st.sidebar.text("No data available")
+			NA = True
+		else:
+			st.sidebar.text("Data is available")
+			cb.plot_lc(lc1)
+			NA = False
+			if st.sidebar.checkbox('Save to Database  (Optional)'):
+				index = str(round(lc1.RAdeg[0],3))+'_'+ str(round(lc1.DEdeg[0],3))
+				lc1.to_sql(index, sqlite_connection, if_exists = 'replace')
+
+
+	if st.sidebar.checkbox('Predict') and ('lc1' in locals()):
+		prediction(lc1,NA)
+		
+
+
+if st.sidebar.checkbox('Xiaodian Chen Dataset'):
+	st.sidebar.text('''Use Source ID to query dataset
+from http://variables.cn:88/
+SourceID range = 1 to 781602
+''' )
+	ID = st.sidebar.number_input('SourceID (integer)', min_value=1, max_value=781602, value=1, step=1)
+	if st.sidebar.checkbox('Query ID'):
+		lc2, lc_complete2 = cb.query_lightcurve_XD(ID)
+		if lc2.empty:
+			st.sidebar.text("No data available")
+			NA = True
+		else:
+			st.sidebar.text("Data is available")
+			cb.plot_lc(lc2)
+			NA = False
+			if st.sidebar.checkbox('Save to Database (Optional)'):
+				index = str(round(lc2.RAdeg[0],3))+'_'+ str(round(lc2.DEdeg[0],3))
+				lc2.to_sql(index, sqlite_connection, if_exists = 'replace')
+	if st.sidebar.checkbox('Predict') and ('lc2' in locals()):
+		prediction(lc2, NA)
+
+
+		
+
+
+
+
+
+
+
+
+
+
---
+++ ui_streamlit/classification_backend.py
@@ -0,0 +1,297 @@
+# classification backend
+# Load libraries
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle # allows to save differnt trained models of the same classifier object
+import time
+import streamlit as st
+
+
+@st.cache
+def query_lightcurve_XD(SourceID): 
+    """
+    Download data for a single source from Xiao Dian's website. Source is identified using SourceID
+    """
+    url = 'http://variables.cn:88/seldataz.php?SourceID=' + str(SourceID)   
+    try:
+        lc_complete = pd.read_csv(url, header='infer')
+        lc = lc_complete.drop(columns = ['SourceID','flag'])
+    except:
+        lc_complete = pd.DataFrame()
+        lc = pd.DataFrame()
+    return lc, lc_complete
+
+
+@st.cache
+def query_lightcurve_DR(RA, Dec): 
+    """
+    Download data for a single source from DR2 dataset. Source is identified using RA and Dec location
+    """
+    circle_radius = 0.0028 # 1 arcsec = 0.00028 degress
+    t_format = "ipac_table"
+    table_format = "FORMAT=" + str(t_format)
+    flag_mask = 32768
+    mask = "BAD_CATFLAGS_MASK=" + str(flag_mask)
+    collect="COLLECTION="+"ztf_dr2"
+    numobs = "NOBS_MIN=20"
+#     filter_band = "g"
+    label = []
+    SourceID =[]
+    start_time = time.time()
+    ra = RA
+    dec = Dec
+    circle = "POS=CIRCLE"+"+"+str(ra)+"+"+str(dec)+"+"+str(circle_radius)
+#     band = "BANDNAME="+ filter_band
+    params = circle + "&" +  mask + "&" + numobs + "&" + collect + "&" + table_format
+
+    try:
+        url= "https://irsa.ipac.caltech.edu/cgi-bin/ZTF/nph_light_curves?" + params
+        lc_complete = pd.read_csv(url, header=None, delim_whitespace=True, skiprows=55) # extract data
+        header = pd.read_csv(url, header=None, sep='|', skiprows=50,usecols=range(1,25), nrows=1)
+        lc_complete.columns = header.iloc[0].str.strip()
+        lc = lc_complete[['ra','dec','hjd','mag','magerr','filtercode']]
+        lc.columns=['RAdeg', 'DEdeg', 'HJD', 'mag', 'e_mag', 'band']
+        lc.replace({'zg':'g'},inplace = True)
+        lc.replace({'zr':'r'},inplace = True)
+        lc.iloc['HJD',:] = lc.iloc['HJD',:]-2.4e6
+    except:
+        lc_complete = pd.DataFrame()
+        lc = pd.DataFrame()
+
+    return lc, lc_complete
+
+
+def plot_lc(lc):
+    """
+    Function to plot the light curves
+    """
+    data1 = lc[lc['band']=='r']
+    data2 = lc[lc['band']=='g']
+
+    fig, axs = plt.subplots(nrows=1, ncols=2, sharex=True)
+    ax = axs[0]
+    ax.errorbar(data1['HJD'],data1['mag'],yerr = data1['e_mag'],fmt='r.')
+    ax.invert_yaxis() # smaller magnitude means brighter stars, so invert the axis
+    ax.set_xlabel ('time in HJD')
+    ax.set_ylabel('magnitude')
+    ax.set_title('Red Filter')
+
+    ax = axs[1]
+    ax.errorbar(data2['HJD'],data2['mag'],yerr = data2['e_mag'],fmt='g.')
+    ax.invert_yaxis() # smaller magnitude means brighter stars, so invert the axis
+    ax.set_xlabel('time in HJD')
+    ax.set_ylabel('magnitude')
+    ax.set_title('Green filter')
+
+    fig.tight_layout(pad=3.0)
+    fig.suptitle('Measured Light Curve', fontsize=16)
+    st.pyplot(fig)
+
+
+def weighted_mean(mag,mag_err):
+    mag2 = (mag_err*mag_err) # mag err square
+    mag2_inv = 1/mag2.values; # take inverse of the values
+    w = pd.Series(mag2_inv) # covert it back to s series
+    sw = w.sum() # sum of weights
+    wmag = mag*w # multiply magnitude with weights
+    wmean = wmag.sum()/sw # weighted mean
+    return wmean
+
+
+# welsh J, K statistics
+def welsh_staton(mag_series,wmean):
+    N = len(mag_series)
+    d_i = N/(N-1)*(mag_series - wmean) # replace mean by weighted mean
+    d_i1 = d_i.shift(periods=-1)
+    d_i1.fillna(0, inplace = True)
+    Pi = d_i*d_i1
+    Pi_val = Pi.values
+    Psign = np.sign(Pi_val)
+    Jval = Psign*np.sqrt(np.abs(Pi_val))
+    J = np.sum(Jval) 
+    K1 = abs(d_i.values)/N
+    K2 = np.sqrt(1/N*np.sum(d_i.values*d_i.values))
+    K = np.sum(K1*K2)
+    return J, K
+
+
+def calculate_features(lc):
+    """
+    Calculate features for a light curve passed as a dataframe.
+    """
+    g_mean = []
+    g_wmean = [] # weighted mean
+    g_MAD = []
+    g_IQR = []
+    g_f60 = []
+    g_f70 = []
+    g_f80 = []
+    g_f90 = []
+    g_skew = []
+    g_kurtosis = []
+    g_welsh_K = []
+    g_welsh_J = []
+
+    r_mean = []
+    r_wmean = [] # weighted mean
+    r_MAD = []
+    r_IQR = []
+    r_f60 = []
+    r_f70 = []
+    r_f80 = []
+    r_f90 = []
+    r_skew = []
+    r_kurtosis = []
+    r_welsh_K = []
+    r_welsh_J = []
+    
+    if len(lc) >1:
+        
+        dfg = lc.loc[lc["band"] == "g"]
+        dfr = lc.loc[lc["band"] == "r"]
+        if len(dfg) > 1:
+            N = len(dfg)
+            wmean_temp = weighted_mean(dfg.mag,dfg.e_mag)
+            K_temp, J_temp =  welsh_staton(dfg.mag, wmean_temp )
+            g_mean.append(dfg.mag.mean())
+            g_wmean.append(wmean_temp) 
+            deviation = abs(dfg.mag - dfg.mag.median())
+            g_MAD.append(deviation.median())
+            g_IQR.append(dfg.mag.quantile(0.75) - dfg.mag.quantile(0.25))
+            g_f60.append(dfg.mag.quantile(0.80) - dfg.mag.quantile(0.2))
+            g_f70.append(dfg.mag.quantile(0.85) - dfg.mag.quantile(0.15))
+            g_f80.append(dfg.mag.quantile(0.9) - dfg.mag.quantile(0.10))
+            g_f90.append(dfg.mag.quantile(0.95) - dfg.mag.quantile(0.05))
+            g_skew.append(dfg.mag.skew())
+            g_kurtosis.append(dfg.mag.kurtosis())
+            g_welsh_J.append(J_temp)
+            g_welsh_K.append(K_temp)
+        else:
+            g_mean.append(np.NaN)
+            g_wmean.append(np.NaN) 
+            g_MAD.append(np.NaN)
+            g_IQR.append(np.NaN)
+            g_f60.append(np.NaN)
+            g_f70.append(np.NaN)
+            g_f80.append(np.NaN)
+            g_f90.append(np.NaN)
+            g_skew.append(np.NaN)
+            g_kurtosis.append(np.NaN)
+            g_welsh_J.append(np.NaN)
+            g_welsh_K.append(np.NaN)
+                
+        if len(dfr) >1:
+            N = len(dfr)
+            wmean_temp = weighted_mean(dfr.mag,dfr.e_mag)
+            K_temp, J_temp =  welsh_staton(dfr.mag, wmean_temp )
+            r_mean.append(dfr.mag.mean())
+            r_wmean.append(wmean_temp) 
+            deviation = abs(dfr.mag - dfr.mag.median())
+            r_MAD.append(deviation.median())
+            r_IQR.append(dfr.mag.quantile(0.75) - dfr.mag.quantile(0.25))
+            r_f60.append(dfr.mag.quantile(0.80) - dfr.mag.quantile(0.2))
+            r_f70.append(dfr.mag.quantile(0.85) - dfr.mag.quantile(0.15))
+            r_f80.append(dfr.mag.quantile(0.9) - dfr.mag.quantile(0.10))
+            r_f90.append(dfr.mag.quantile(0.95) - dfr.mag.quantile(0.05))
+            r_skew.append(dfr.mag.skew())
+            r_kurtosis.append(dfr.mag.kurtosis())
+            r_welsh_J.append(J_temp)
+            r_welsh_K.append(K_temp)
+        else:
+            r_mean.append(np.NaN)
+            r_wmean.append(np.NaN) 
+            r_MAD.append(np.NaN)
+            r_IQR.append(np.NaN)
+            r_f60.append(np.NaN)
+            r_f70.append(np.NaN)
+            r_f80.append(np.NaN)
+            r_f90.append(np.NaN)
+            r_skew.append(np.NaN)
+            r_kurtosis.append(np.NaN)
+            r_welsh_J.append(np.NaN)
+            r_welsh_K.append(np.NaN)
+
+    else:
+        g_mean.append(np.NaN)
+        g_wmean.append(np.NaN) 
+        g_MAD.append(np.NaN)
+        g_IQR.append(np.NaN)
+        g_f60.append(np.NaN)
+        g_f70.append(np.NaN)
+        g_f80.append(np.NaN)
+        g_f90.append(np.NaN)
+        g_skew.append(np.NaN)
+        g_kurtosis.append(np.NaN)
+        g_welsh_J.append(np.NaN)
+        g_welsh_K.append(np.NaN)
+        r_mean.append(np.NaN)
+        r_wmean.append(np.NaN) 
+        r_MAD.append(np.NaN)
+        r_IQR.append(np.NaN) 
+        r_f60.append(np.NaN)
+        r_f70.append(np.NaN)
+        r_f80.append(np.NaN)
+        r_f90.append(np.NaN)
+        r_skew.append(np.NaN)
+        r_kurtosis.append(np.NaN)
+        r_welsh_J.append(np.NaN)
+        r_welsh_K.append(np.NaN)
+        
+    # del features
+    features = pd.DataFrame()
+    N = 1
+
+    # g filter data
+    features['g_mean'] = g_mean[0:N]
+    features['g_wmean'] = g_wmean[0:N]
+    features['g_MAD'] = g_MAD[0:N]
+    features['g_IQR'] = g_IQR[0:N]
+    features['g_f60'] = g_f60[0:N]
+    features['g_f70'] = g_f70[0:N]
+    features['g_f80'] = g_f80[0:N]
+    features['g_f90'] = g_f90[0:N]
+    features['g_skew'] = g_skew[0:N]
+    features['g_kurtosis'] = g_kurtosis[0:N]
+    features['g_welsh_J'] = g_welsh_J[0:N]
+    features['g_welsh_K'] = g_welsh_K[0:N]
+
+    # r filter data
+    features['r_mean'] = r_mean[0:N]
+    features['r_wmean'] = r_wmean[0:N]
+    features['r_MAD'] = r_MAD[0:N]
+    features['r_IQR'] = r_IQR[0:N]
+    features['r_f60'] = r_f60[0:N]
+    features['r_f70'] = r_f70[0:N]
+    features['r_f80'] = r_f80[0:N]
+    features['r_f90'] = r_f90[0:N]
+    features['r_skew'] = r_skew[0:N]
+    features['r_kurtosis'] = r_kurtosis[0:N]
+    features['r_welsh_J'] = r_welsh_J[0:N]
+    features['r_welsh_K'] = r_welsh_K[0:N]
+
+    return features
+
+
+
+def prediction_probabilty(features):
+    """
+    Predict probability for each of the 9 variable types using pre calculated features.
+    """
+    prob={}
+    label = ['BYDra', 'EW', 'SR', 'RSCVN', 'RR', 'DSCT', 'EA', 'Mira', 'RRc']
+    prob_pd = pd.DataFrame(columns=['Probability'],index=label)
+    if np.isnan(features.iloc[0,:].values).all():
+        pass
+    else:
+        for variable_type in label:
+            print(variable_type)
+            name = 'XGBoost'
+            filename = '../pickles/'+ name+'_'+variable_type+'.pkl'
+            clf = pickle.load(open(filename, 'rb'))
+            predict_proba = clf.predict_proba(features)
+            prob[variable_type] = round(predict_proba[0,0],2)
+    #         prob[variable_type] = clf.predict_proba(features)
+        prob_pd['Probability']=prob.values()
+    return prob_pd
